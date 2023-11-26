1866ade99cfeccdc9ce53a52f42654122c1c44ce
yite <yite@orichal.io>
2023-11-26 21:59:58 +0800

    publisher

---
+++ publisher/redis_publisher.py
@@ -0,0 +1,9 @@
+import redis
+
+
+class Publisher:
+    def __init__(self, host='localhost', port=6379):
+        self.redis = redis.Redis(host=host, port=port)
+
+    def publish(self, channel, message):
+        self.redis.publish(channel, message)
