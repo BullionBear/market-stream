c1811b9c038aeafab69724b86170e3aedff5de2e
yite <coastq22889@icloud.com>
2023-11-29 06:41:04 +0800

    rename library

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='market-stream',
-    version='0.0.2',
+    version='0.0.3',
     description='A centralized market stream manager',
     author='Yi Te',
     author_email='coastq22889@icloud.com',
-    packages=find_packages(include=['client', 'generated']),
+    packages=find_packages(include=['market_stream', 'generated', "helper"]),
     install_requires=[
         'websockets',
         'redis',
